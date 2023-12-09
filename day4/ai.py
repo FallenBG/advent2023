58f4e4b46d82c95afc9808098d8fd5ebf3f895f0
mdaskalov <thefallenangel.bg@gmail.com>
2023-12-09 23:53:11 +0800

    Day 4 task 2

--- day4/ai.py
+++ day4/ai.py
@@ -11,30 +11,48 @@ def calculate_points_from_file(filename):
         with open(filename, 'r') as file:
             lines = file.readlines()
 
-        # Function to calculate points for each scratchcard
-        def calculate_points(scratchcard):
-            # Extract the part after the colon and split the winning numbers and the numbers you have
-            winning_numbers, your_numbers = scratchcard.split(': ')[1].split(' | ')
+        # Extract the scratchcards and their details
+        scratchcards = []
+        for line in lines:
+            parts = line.split(': ')
+            # Ensure that the card number part is not empty and is a valid number
+            # Using strip() to remove extra spaces and then split
+            card_number = int(parts[0].replace('Card', '').strip())
+
+            winning_numbers, your_numbers = parts[1].split(' | ')
             winning_numbers = set(map(int, winning_numbers.split()))
+
             your_numbers = set(map(int, your_numbers.split()))
+            scratchcards.append((card_number, winning_numbers, your_numbers))
 
-            # Count the matches
-            matches = winning_numbers.intersection(your_numbers)
-            if len(matches) == 0:
-                return 0
-            else:
-                # Calculate the points
-                return 2 ** (len(matches) - 1)
+        # Function to calculate matches for each scratchcard
+        def calculate_matches(winning_numbers, your_numbers):
+            return len(winning_numbers.intersection(your_numbers))
 
-        # Calculate the total points
-        total_points = sum(calculate_points(card) for card in lines)
-        return total_points
+        # Initialize the count of each card (originals)
+        card_counts = {card_number: 1 for card_number, _, _ in scratchcards}
+
+        # Process each card
+        for i in range(len(scratchcards)):
+            current_card, winning_numbers, your_numbers = scratchcards[i]
+            matches = calculate_matches(winning_numbers, your_numbers)
+
+            # Win copies of the next cards equal to the number of matches
+            for j in range(i + 1, min(i + 1 + matches, len(scratchcards))):
+                next_card, _, _ = scratchcards[j]
+                card_counts[next_card] += card_counts[current_card]
+
+        # Calculate the total number of scratchcards
+        total_scratchcards = sum(card_counts.values())
+        return total_scratchcards
 
     except FileNotFoundError:
         return "File not found."
+    except ValueError as e:
+        return str(e)
 
 
-# print(calculate_points_from_file('input'))
+print(calculate_points_from_file('input'))
 for i in range(1000):
     calculate_points_from_file("input")
 
--- day4/main.py
+++ day4/main.py
@@ -4,25 +4,38 @@ start_time = time.time()
 
 
 def main():
-    total = 0
+    cards = {}
     with open("input", 'r') as file:
+        i = 1
         for row in file:
             row = row.replace('\n', '').split(': ')[1]
             wining = row.split(' | ')[0].strip().replace('  ', ' ').split(' ')
             numbers = row.split(' | ')[1].strip().replace('  ', ' ').split(' ')
 
-            matcing = set(wining) & set(numbers)
-            # print(generate_geometric_progression(len(matcing)))
-            total += generate_geometric_progression(len(matcing))
+            matcing = len(set(wining) & set(numbers))
+
+            cards[i] = {
+                "row": i,
+                "cards": 1,
+                "winnings": matcing
+            }
+            i += 1
+
+    total = 0
+    for key, val in cards.items():
+        if val["winnings"] > 0:
+            add_cards(val, key, cards)
+
+
+    for key, val in cards.items():
+        total += val['cards']
 
     return total
 
 
-def generate_geometric_progression(num_terms):
-    if num_terms == 0:
-        return 0
-    else:
-        return 1 * 2**(num_terms - 1)
+def add_cards(val, key, cards):
+    for i in range(val["winnings"]):
+        cards[key+i+1]['cards'] += val['cards']
 
 
 # print(main())
